eb5b2c56f6e22d7fff3064758385df2503b0800c
mpcti <39888578+mpcti@users.noreply.github.com>
2018-06-04 21:18:21 -0300

    com Webview Funcionando

--- app/src/main/java/com/example/marcio/webservice/Adapter.java
+++ app/src/main/java/com/example/marcio/webservice/Adapter.java
@@ -4,6 +4,8 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
 import android.widget.BaseAdapter;
 import android.widget.TextView;
 
@@ -39,6 +41,9 @@ public class Adapter extends BaseAdapter {
         LayoutInflater layoutInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         view = layoutInflater.inflate(R.layout.item_listview_p,null);
     }
+
+
+        WebView um = view.findViewById(R.id.img);
         TextView umtitlle = view.findViewById(R.id.tw2);
         TextView umimage = view.findViewById(R.id.tw4);
         TextView umrating = view.findViewById(R.id.tw6);
@@ -47,11 +52,19 @@ public class Adapter extends BaseAdapter {
         TextView doisgenero = view.findViewById(R.id.twg2);
         TextView tresgenero = view.findViewById(R.id.twg3);
 
+
+
         Movies movies = (Movies) getItem(i);
          umtitlle.setText(movies.getTitle());
          umimage.setText(movies.getImage());
          umrating.setText(String.valueOf(movies.getRating()));
          umrealeseyear.setText( String.valueOf(movies.getReleaseYear()));
+
+
+        um.getSettings().setJavaScriptEnabled(true);
+        um.getSettings().setSupportZoom(false);
+        um.loadUrl(movies.getImage());
+
         String p[];
         p = new Operacaodao(context).listarfilmeporgenero(movies.getId_filme());
         movies.setGenre(p);
@@ -78,3 +91,5 @@ public class Adapter extends BaseAdapter {
         return view;
     }
 }
+
+
